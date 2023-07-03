7aa7c3dd19a9a663a5cb1563fbab02e1f328295a
mitchel buyengo <mitchelongachi63@gmail.com>
2023-07-03 13:29:52 +0300

    completed work

--- index.js
+++ index.js
@@ -1,17 +1,13 @@
-function introduction(Aki){
-    return `Hi, my name is ${Aki}.`;
-}
-
-function introductionWithLanguage(name, language){
-    if (language==='Ember.js'){
-        return "Hi, my name is Aki and I am learning to program in Ember.js."
-    }
-    else if (language==='react'){
-        return "Hi my name is Samip and I  am learning to program in React."
-    }
-    introductionWithLanguage('Samip','react');
-    
-}function introductionWithLanguageOptional(name, language='Python'){
-    return 'Hi, my name is Gracie and I am learning to program in Python.';
-}
-
+function introduction(name){
+    return `Hi, my name is ${name}.`;
+ }
+ 
+ function introductionWithLanguage(name, language){
+     return `Hi, my name is ${name} and I am learning to program in ${language}.`;
+ }
+ 
+ function introductionWithLanguageOptional(name, language ="JavaScript"){
+     return `Hi, my name is ${name} and I am learning to program in ${language}.`;
+ }
+ 
+ introductionWithLanguageOptional()
